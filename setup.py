5100272d14f07908d7b0eba60f0ac0f7dc19759d
Nick80835 <nick80835@gmail.com>
2020-10-02 13:15:02 -0400

    rename package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="asyncbooru-nick80835",
+    name="asyncbooru",
     version="0.0.1",
     author="Nick80835",
     author_email="nick80835@gmail.com",
