78f202e08e2d875d3b346149400984a7c65078ec
Nick80835 <nick80835@gmail.com>
2020-10-03 09:30:42 -0400

    add sankaku complex beta

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 This project exists simply because I want to learn new things.
 
 ## To-do list
-* Add support for Sankaku Complex
 * Add support for Konachan
 * Maybe add support for auth
--- asyncbooru/__init__.py
+++ asyncbooru/__init__.py
@@ -1,3 +1,4 @@
 from .danbooru import Danbooru, DanbooruPost
 from .gelbooru import Gelbooru, GelbooruPost
+from .sankaku import Sankaku, SankakuPost
 from .yandere import Yandere, YanderePost
